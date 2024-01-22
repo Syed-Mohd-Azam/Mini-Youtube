6bcd40d4f9a858a162d712d452b2df793f683cb1
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-01-23 00:42:30 +0530

    Hamberger Menu and Youtube Icon added into the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
+import Hamberger from "../assets/Hamberger-menu-icon.png";
+import YoutubeIcon from "../assets/Youtube-Icon.png";
 const Header = () => {
   return (
     <>
-      <div>Header</div>
+      <section>
+        <article className="flex">
+          <img src={Hamberger} alt="Hamberger-Menu-Icon" />
+          <img src={YoutubeIcon} alt="Youtube-Icon" />
+        </article>
+      </section>
     </>
   );
 };
