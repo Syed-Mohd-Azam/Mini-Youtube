dd315aa3ded8e3050511d9964c52d09ae1fcd980
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-01-23 02:18:27 +0530

    Header Component fully designed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,33 @@
 import Hamberger from "../assets/Hamberger-menu-icon.png";
 import YoutubeIcon from "../assets/youtube.png";
+import UserIcon from "../assets/user-icon.jpg";
+import Search from "../assets/search.png";
 const Header = () => {
   return (
     <>
-      <section>
-        <article className="flex">
+      <section className="grid grid-flow-col shadow-md p-3">
+        <article className="flex gap-3 col-span-1  justify-start items-center px-2">
           <img
-            className="w-8 h-8 hover:cursor-pointer"
+            className="w-8 h-8 hover:cursor-pointer pt-2"
             src={Hamberger}
             alt="Hamberger-Menu-Icon"
           />
-          <img className="w-28 " src={YoutubeIcon} alt="Youtube-Icon" />
+          <img className="w-24 " src={YoutubeIcon} alt="Youtube-Icon" />
+        </article>
+        <article className="col-span-10 flex justify-center items-center">
+          <section className="flex justify-content items-center gap-0 border-2 rounded-e-full rounded-l-full p-2">
+            <input
+              type="text"
+              className="border-none w-96 rounded-2xl outline-none pl-1"
+              placeholder="Search"
+            />
+            <article className="pr-1e">
+              <img className="w-8 h-8" src={Search} alt="search" />
+            </article>
+          </section>
+        </article>
+        <article className="col-span-1 flex justify-center">
+          <img className="w-12 h-12" src={UserIcon} alt="User-Icon" />
         </article>
       </section>
     </>
