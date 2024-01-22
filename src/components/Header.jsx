371352134ea0171f7f40eedda2588071ffe198f9
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-01-23 00:52:44 +0530

    youtube icon changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 import Hamberger from "../assets/Hamberger-menu-icon.png";
-import YoutubeIcon from "../assets/Youtube-Icon.png";
+import YoutubeIcon from "../assets/youtube.png";
 const Header = () => {
   return (
     <>
       <section>
         <article className="flex">
-          <img src={Hamberger} alt="Hamberger-Menu-Icon" />
-          <img src={YoutubeIcon} alt="Youtube-Icon" />
+          <img
+            className="w-8 h-8 hover:cursor-pointer"
+            src={Hamberger}
+            alt="Hamberger-Menu-Icon"
+          />
+          <img className="w-28 " src={YoutubeIcon} alt="Youtube-Icon" />
         </article>
       </section>
     </>
