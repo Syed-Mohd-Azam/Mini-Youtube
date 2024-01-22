3ded8d3d20050ae4796a2063a3ecf4ba4405e841
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-01-22 22:06:27 +0530

    tailwind css installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import "./App.css";
-
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1 className="text-5xl text-red-600 align-middle">React </h1>
+    </>
+  );
 }
 
 export default App;
