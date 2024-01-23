98c5613f09a0cd98ccd745827e949aedf4a8b9b6
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-01-24 00:12:37 +0530

    Providing store to the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { Provider } from "react-redux";
 import Body from "./components/Body";
 import Header from "./components/Header";
+import store from "./utils/store";
 const App = () => {
   return (
     <>
-      <Header />
-      <Body />
+      <Provider store={store}>
+        <Header />
+        <Body />
+      </Provider>
     </>
   );
 };
