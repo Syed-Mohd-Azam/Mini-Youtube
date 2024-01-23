dc95d369cd80d335d9d5e646ac3ab870fffbf230
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-01-24 03:11:20 +0530

    HalfSideBar Component Completed

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,11 +3,11 @@ import SideBar from "./SideBar";
 const Body = () => {
   return (
     <>
-      <section className="grid grid-flow-col grid-cols-14">
+      <section className="grid grid-flow-col grid-cols-12">
         <article className="col-span-2 border-r-2 p-4">
           <SideBar />
         </article>
-        <article className="col-span-12 p-4">
+        <article className="col-span-10 p-4">
           <MainContainer />
         </article>
       </section>
--- src/components/HalfSideBarSection.jsx
+++ src/components/HalfSideBarSection.jsx
@@ -1,7 +1,37 @@
+import Home from "../assets/Home.png";
+import Shorts from "../assets/shorts.png";
+import Subscription from "../assets/subscription.png";
+import You from "../assets/You.png";
+import Download from "../assets/Download.png";
 const HalfSideBarSection = () => {
   return (
     <>
-      <section></section>
+      <section className="flex  gap-4 flex-col min-h-screen ">
+        <article className="flex justify-start gap-4 items-center hover:bg-purple-50 hover:rounded-2xl py-1">
+          <img className="h-16 w-16" src={Home} alt="Home-Icon" />
+          <p className=" text-lg">Home</p>
+        </article>
+        <article className="flex justify-start gap-4 items-center hover:bg-purple-50 hover:rounded-2xl py-1">
+          <img className="h-14 w-14 p-2" src={Shorts} alt="shorts" />
+          <p className=" text-lg">Shorts</p>
+        </article>
+        <article className="flex justify-start gap-4 items-center hover:bg-purple-50 hover:rounded-2xl py-1">
+          <img
+            className="h-11 w-11 p-2"
+            src={Subscription}
+            alt="subscriptions"
+          />
+          <p className=" text-lg p-3">Subscriptions</p>
+        </article>
+        <article className="flex justify-start gap-4 items-center hover:bg-purple-50 hover:rounded-2xl py-1">
+          <img className="h-11 w-11 p-2" src={You} alt="subscriptions" />
+          <p className="text-lg p-2">You</p>
+        </article>
+        <article className="flex justify-start gap-4 items-center hover:bg-purple-50 hover:rounded-2xl py-1">
+          <img className="h-11 w-11 p-2" src={Download} alt="subscriptions" />
+          <p className="text-lg p-2">Downloads</p>
+        </article>
+      </section>
     </>
   );
 };
