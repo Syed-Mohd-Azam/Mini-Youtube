dfe8fd4c2cc1e719b4b362c493a8c44c01917580
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-01-25 04:03:24 +0530

    changes in sizing

--- src/components/HalfSideBarSection.jsx
+++ src/components/HalfSideBarSection.jsx
@@ -9,27 +9,27 @@ const HalfSideBarSection = () => {
       <section className="flex  flex-col min-h-screen max-h-fit ">
         <article className="flex justify-start gap-4 items-center hover:bg-purple-50 hover:rounded-lg ">
           <img className="h-16 w-16" src={Home} alt="Home-Icon" />
-          <p className=" text-lg">Home</p>
+          <p className=" text-md">Home</p>
         </article>
         <article className="flex justify-start gap-4 items-center hover:bg-purple-50 hover:rounded-lg mb-6">
-          <img className="h-10 w-10 ml-3" src={Shorts} alt="shorts" />
-          <p className=" text-lg ml-3">Shorts</p>
+          <img className="h-8 w-8 ml-3" src={Shorts} alt="shorts" />
+          <p className=" text-md ml-3">Shorts</p>
         </article>
         <article className="flex justify-start gap-4 items-center hover:bg-purple-50 hover:rounded-lg mb-6 ">
           <img
-            className="h-8 w-8 ml-4"
+            className="h-6 w-6 ml-4"
             src={Subscription}
             alt="subscriptions"
           />
-          <p className=" text-lg ml-4">Subscriptions</p>
+          <p className=" text-md ml-4">Subscriptions</p>
         </article>
         <article className="flex justify-start gap-4 items-center hover:bg-purple-50 hover:rounded-lg mb-6">
-          <img className="h-7 w-7  ml-4" src={You} alt="subscriptions" />
-          <p className="text-lg ml-5">You</p>
+          <img className="h-6 w-6  ml-4" src={You} alt="subscriptions" />
+          <p className="text-md ml-5">You</p>
         </article>
         <article className="flex justify-start gap-4 items-center hover:bg-purple-50 hover:rounded-lg mb-6">
           <img className="h-6 w-6  ml-4" src={Download} alt="subscriptions" />
-          <p className="text-lg ml-5">Downloads</p>
+          <p className="text-md ml-6">Downloads</p>
         </article>
       </section>
     </>
