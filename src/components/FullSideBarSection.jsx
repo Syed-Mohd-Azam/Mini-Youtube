d404fb49137175ffddf986be8151b23720843c44
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-01-25 01:07:38 +0530

    images replaced

--- src/components/FullSideBarSection.jsx
+++ src/components/FullSideBarSection.jsx
@@ -2,6 +2,10 @@ import Home from "../assets/Home.png";
 import Shorts from "../assets/shorts.png";
 import Subscription from "../assets/subscription.png";
 import Download from "../assets/download.png";
+import Channel from "../assets/channel.png";
+import History from "../assets/history.png";
+import Videos from "../assets/videos.png";
+import Watch from "../assets/watch-later.png";
 const FullSideBarSection = () => {
   return (
     <>
@@ -26,29 +30,25 @@ const FullSideBarSection = () => {
         </article>
         <article className="border-b border-1 mt-5"></article>
         <p className="font-semibold ml-4 mt-4 text-lg mb-5">You {">"}</p>
-        <section>
-          <article className="flex gap-6">
-            <img
-              className="h-10 w-10"
-              src={Download}
-              alt="Your-Channel-Image"
-            />
+        <section className="flex flex-col gap-3 p-2">
+          <article className="flex gap-6 mb-1">
+            <img className="h-6 w-6" src={Channel} alt="Your-Channel-Image" />
             <p>Your Channel</p>
           </article>
           <article className="flex gap-7 mb-2">
-            <img className="h-10 w-10" src={Download} alt="History-Image" />
+            <img className="h-6 w-6" src={History} alt="History-Image" />
             <p>History</p>
           </article>
           <article className="flex gap-7 mb-2">
-            <img className="h-10 w-10" src={Download} alt="Videos-Image" />
+            <img className="h-6 w-6" src={Videos} alt="Videos-Image" />
             <p>Your Videos</p>
           </article>
           <article className="flex gap-7 mb-2">
-            <img className="h-10 w-10" src={Download} alt="Watch-Later-Image" />
+            <img className="h-6 w-6" src={Watch} alt="Watch-Later-Image" />
             <p>Watch later</p>
           </article>
           <article className="flex gap-7 mb-2">
-            <img className="w-10 h-10" src={Download} alt="Download-Image" />
+            <img className="w-6 h-6" src={Download} alt="Download-Image" />
             <p>Downloads</p>
           </article>
         </section>
