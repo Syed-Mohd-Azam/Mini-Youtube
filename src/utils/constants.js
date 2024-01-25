7955b524ac3a41a4806c6484377674a252b867f9
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-01-26 00:15:52 +0530

    API_KEY changed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const YOUTUBE_API_KEY = "AIzaSyCZucDDpJKXjHTy46XM-cTHhWXIoYID2Y8";
+const YOUTUBE_API_KEY = "AIzaSyD5o_WlNFauPuRtVsZIKOm9LABtFJwDYtk";
 const YOUTUBE_VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=";
 export const API = YOUTUBE_VIDEOS_API + YOUTUBE_API_KEY;
