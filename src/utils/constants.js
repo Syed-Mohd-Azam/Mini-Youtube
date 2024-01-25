48c8b507543fcec808e387b86d1f854bd4867e33
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-01-26 04:53:31 +0530

    maxResults parameter changed to fetch the maximum videos

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 const YOUTUBE_API_KEY = "AIzaSyD5o_WlNFauPuRtVsZIKOm9LABtFJwDYtk";
 const YOUTUBE_VIDEOS_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&maxResults=50&chart=mostPopular&regionCode=US&key=";
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&maxResults=48&chart=mostPopular&regionCode=US&key=";
 export const API = YOUTUBE_VIDEOS_API + YOUTUBE_API_KEY;
