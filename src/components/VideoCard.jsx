ce7c2a37097389182d972036bb57cc7ccad0e88c
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-01-26 02:06:10 +0530

    VideosCard Structure Completed

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,6 +1,15 @@
+/* eslint-disable react/prop-types */
 // eslint-disable-next-line react/prop-types
 const VideoCard = ({ item }) => {
   console.log(item);
-  return <>VIDE0CARD</>;
+  const {
+    // eslint-disable-next-line react/prop-types
+    snippet: {
+      thumbnails: {
+        medium: { url },
+      },
+    },
+  } = item;
+  return <>VIDE0CARD </>;
 };
 export default VideoCard;
