84c8694a171774b940533e0031365665a90126a9
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-01-27 01:19:14 +0530

    created WatchPage Component

--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -1,11 +1,4 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { closeMenu } from "../utils/appSlice";
 const WatchPage = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(closeMenu);
-  }, []);
   return (
     <>
       <h1>Watch Page</h1>
