f9b0f97bdcbe1f5c361751fce3ce28a2a04d6cde
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-01-28 00:41:27 +0530

    react-icons library installed

--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -1,21 +1,27 @@
 import buttons from "../utils/buttons";
 import Button from "./Button";
 import { useState } from "react";
+import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
+import { MdOutlineArrowRight } from "react-icons/md";
 const ButtonList = () => {
   const [chance, setChance] = useState(0);
   return (
     <>
       <section className=" pb-5 w-11/12 mx-auto">
-        <article className="flex flex-row overflow-x-auto scrollbar-hide flex-nowrap scroll-smooth  ">
-          {buttons?.map((button, index) => (
-            <Button
-              index={index}
-              chance={chance}
-              key={index}
-              button={button}
-              setChance={setChance}
-            />
-          ))}
+        <article className="flex flex-row gap-1">
+          <MdOutlineKeyboardArrowLeft />
+          <article className="flex flex-row overflow-x-auto scrollbar-hide flex-nowrap scroll-smooth  ">
+            {buttons?.map((button, index) => (
+              <Button
+                index={index}
+                chance={chance}
+                key={index}
+                button={button}
+                setChance={setChance}
+              />
+            ))}
+          </article>
+          <MdOutlineArrowRight />
         </article>
       </section>
     </>
