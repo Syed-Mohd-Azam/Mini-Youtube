f98e745a2d55ca7820f7ee486f278d5360692258
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-01-28 00:51:20 +0530

    Arrow Icons placed perfectly in ButtonList Component

--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -2,14 +2,14 @@ import buttons from "../utils/buttons";
 import Button from "./Button";
 import { useState } from "react";
 import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
-import { MdOutlineArrowRight } from "react-icons/md";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 const ButtonList = () => {
   const [chance, setChance] = useState(0);
   return (
     <>
       <section className=" pb-5 w-11/12 mx-auto">
-        <article className="flex flex-row gap-1">
-          <MdOutlineKeyboardArrowLeft />
+        <article className="flex flex-row gap-2 justify-center items-center">
+          <MdOutlineKeyboardArrowLeft className="w-8 h-8 hover:bg-purple-100 text-black rounded-full" />
           <article className="flex flex-row overflow-x-auto scrollbar-hide flex-nowrap scroll-smooth  ">
             {buttons?.map((button, index) => (
               <Button
@@ -21,7 +21,7 @@ const ButtonList = () => {
               />
             ))}
           </article>
-          <MdOutlineArrowRight />
+          <MdOutlineKeyboardArrowRight className="w-8 h-8 hover:bg-purple-100 rounded-full" />
         </article>
       </section>
     </>
