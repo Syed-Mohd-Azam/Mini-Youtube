b59abc6dda5eefd81a85d9adbb1a64bd9c6535b4
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-01-27 23:55:33 +0530

    changes added

--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -4,7 +4,7 @@ const ButtonList = () => {
   return (
     <>
       <section className=" pb-5 w-11/12 mx-auto">
-        <article className="flex flex-row overflow-x-auto scrollbar-hide flex-nowrap scroll-smooth scroll-mb-5 ">
+        <article className="flex flex-row overflow-x-auto scrollbar-hide flex-nowrap scroll-smooth  ">
           {buttons?.map((button, index) => (
             <Button key={index} button={button} />
           ))}
