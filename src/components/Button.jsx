6e123b6790d6195deff9db90c04cd08ab5c4c070
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-01-28 00:04:11 +0530

    Color on clicking the button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,14 @@
 // eslint-disable-next-line react/prop-types
-const Button = ({ button }) => {
+const Button = ({ button, chance, index }) => {
   return (
     <>
-      <button className="bg-purple-50 p-3 mx-2 font-semibold rounded-lg w-fit whitespace-nowrap">
+      <button
+        className={
+          chance === index
+            ? "bg-black text-white p-3 mx-2 font-semibold rounded-lg w-fit whitespace-nowrap"
+            : "bg-purple-50 p-3 mx-2 font-semibold rounded-lg w-fit whitespace-nowrap"
+        }
+      >
         {button}
       </button>
     </>
--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -1,12 +1,20 @@
 import buttons from "../utils/buttons";
 import Button from "./Button";
+import { useState } from "react";
 const ButtonList = () => {
+  const [chance, setChance] = useState(0);
   return (
     <>
       <section className=" pb-5 w-11/12 mx-auto">
         <article className="flex flex-row overflow-x-auto scrollbar-hide flex-nowrap scroll-smooth  ">
           {buttons?.map((button, index) => (
-            <Button key={index} button={button} />
+            <Button
+              index={index}
+              chance={chance}
+              key={index}
+              button={button}
+              onClick={() => setChance(index)}
+            />
           ))}
         </article>
       </section>
